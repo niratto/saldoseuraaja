962f9ba61c6159483f9ec881e31a434ea428c7e7
Cola Code-Monkey <g116gfj@kelas127.kela.root>
2018-08-16 15:11:30 +0300

    lisätään viimeisimmät muutokset (heroku --> __init.py__)

--- application/__init__.py
+++ application/__init__.py
@@ -3,12 +3,16 @@ app = Flask(__name__)
 
 # Tuodaan SQLAlchemy käyttöön
 from flask_sqlalchemy import SQLAlchemy
-# Käytetään saldoseuraaja.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
-# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
-# samassa paikassa
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///saldoseuraaja.db"
-# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
-app.config["SQLALCHEMY_ECHO"] = True
+
+if os.environ.get("HEROKU"):
+	app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+	# Käytetään saldoseuraaja.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
+	# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
+	# samassa paikassa
+	app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///saldoseuraaja.db"
+	# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
+	app.config["SQLALCHEMY_ECHO"] = True
 
 # Luodaan db-olio, jota käytetään tietokannan käsittelyyn
 db = SQLAlchemy(app)
@@ -41,4 +45,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 # Luodaan lopulta tarvittavat tietokantataulut
-db.create_all()
+try:
+	db.create_all()
+except:
+	pass
